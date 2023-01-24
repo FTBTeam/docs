713130e4db3f258926027dfe4f30c375d3a38f3a
Michael Hillcox <michaelhillcox@gmail.com>
2023-01-24 10:22:20 +0000

    fix: no robots, no google indexing

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,10 +3,7 @@
  * @author Feed The Beast Ltd
  */
 
-import {
-    defaultTheme,
-    defineUserConfig
-} from 'vuepress'
+import { defaultTheme, defineUserConfig } from "vuepress";
 
 import sidebar from "./app/sidebar/SidebarCreator.js";
 
@@ -16,16 +13,20 @@ import sidebar from "./app/sidebar/SidebarCreator.js";
 
 //The VuePress configuration, where the sidebar variable is used, along other configuration options.
 export default defineUserConfig({
-    lang: 'en-US',
-    title: 'Feed The Beast Docs',
-    description: 'Documentation for content made by Feed The Beast.',
-    theme: defaultTheme({
-        sidebar: sidebar,
-        logo: 'https://vuejs.org/images/logo.png',
-        repo: 'https://github.com/ftbteam/docs',
-        docsRepo: 'https://github.com/ftbteam/docs/docs',
-        docsBranch: 'main',
-        contributors: false,
-        colorMode: "dark"
-    })
-})
+  head: [
+    ["meta", { name: "robots", content: "noindex" }],
+    ["meta", { name: "robots", content: "noindex" }],
+  ],
+  lang: "en-US",
+  title: "Feed The Beast Docs",
+  description: "Documentation for content made by Feed The Beast.",
+  theme: defaultTheme({
+    sidebar: sidebar,
+    logo: "https://vuejs.org/images/logo.png",
+    repo: "https://github.com/ftbteam/docs",
+    docsRepo: "https://github.com/ftbteam/docs/docs",
+    docsBranch: "main",
+    contributors: false,
+    colorMode: "dark",
+  }),
+});
